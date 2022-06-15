a4058ada210255e2d41092ddd7d54ed0fc908ff1
imysh578 <imysh578@gmail.com>
2022-06-16 00:39:38 +0900

    set layouts

---
+++ client/src/components/Clock.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Clock = () => {
+  return (
+		<div className="container">
+			<div className="clock">
+        <span><b>1</b></span>
+      </div>
+		</div>
+	);
+}
+
+export default Clock
