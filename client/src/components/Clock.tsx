80f5b855763ca6695e985e346555a60d8b862f99
imysh578 <imysh578@gmail.com>
2022-06-17 03:00:17 +0900

    done: display analog clock

--- client/src/components/Clock.tsx
+++ client/src/components/Clock.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Hours from './Hours';
 
 const Clock = () => {
+  useEffect(() => {
+    const intervalId = initial();
+    return () => 
+      clearInterval(intervalId)
+  }, [])
+
+  const initial = () => {
+    let hr: HTMLDivElement|null = document.querySelector("#hr");
+    let mn: HTMLDivElement|null = document.querySelector("#mn");
+    let sc: HTMLDivElement|null = document.querySelector("#sc");
+  
+    const intervalId = setInterval(() => {
+      let day: Date = new Date();
+      let hh: number = day.getHours() * 30;
+      let mm: number = day.getMinutes() * 6;
+      let ss: number = day.getSeconds() * 6;
+  
+      if(hr) hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`
+      if(mn) mn.style.transform = `rotateZ(${mm}deg)`
+      if(sc) sc.style.transform = `rotateZ(${ss}deg)`
+    }, 1000)
+
+    return intervalId;
+  }
+
   return (
 		<div className="container">
 			<div className="clock">
-        <span><b>1</b></span>
+        <div className="circle" id='sc' style={{"--clr": "var(--circle1-color)"} as React.CSSProperties}><i></i></div>
+        <div className="circle circle2" id='mn' style={{"--clr": "var(--circle2-color)"} as React.CSSProperties}><i></i></div>
+        <div className="circle circle3" id='hr' style={{"--clr": "var(--circle3-color)"} as React.CSSProperties}><i></i></div>
+        <Hours/>
       </div>
 		</div>
 	);
---
+++ client/src/components/Hours.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hours = () => {
+  return (
+    <>
+      <span style={{"--i": 1} as React.CSSProperties}><b>1</b></span>
+      <span style={{"--i": 2} as React.CSSProperties}><b>2</b></span>
+      <span style={{"--i": 3} as React.CSSProperties}><b>3</b></span>
+      <span style={{"--i": 4} as React.CSSProperties}><b>4</b></span>
+      <span style={{"--i": 5} as React.CSSProperties}><b>5</b></span>
+      <span style={{"--i": 6} as React.CSSProperties}><b>6</b></span>
+      <span style={{"--i": 7} as React.CSSProperties}><b>7</b></span>
+      <span style={{"--i": 8} as React.CSSProperties}><b>8</b></span>
+      <span style={{"--i": 9} as React.CSSProperties}><b>9</b></span>
+      <span style={{"--i": 10} as React.CSSProperties}><b>10</b></span>
+      <span style={{"--i": 11} as React.CSSProperties}><b>11</b></span>
+      <span style={{"--i": 12} as React.CSSProperties}><b>12</b></span>
+    </>
+  )
+}
+
+export default Hours
